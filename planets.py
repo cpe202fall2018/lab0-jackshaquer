8ecb17022f00c677e811755d30ae3f87b1f78a81
Jack Shaquer <jackshaquer@gmail.com>
2018-09-23 11:46:55 -0700

    Lab0 complete

--- planets.py
+++ planets.py
@@ -1,7 +1,12 @@
+# Lab0
+# Name: Jack Shaquer
+# Instructor: Paul Hatalsky
+# Section: 05
+
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+   weight = float(input("What do you weigh on earth? "))
+   print("\n" + "On Mars you would weigh " + format(.38 * weight, ".2f") + " pounds." + "\n" +
+         "On Jupiter you would weigh " + format(2.34 * weight, ".2f") + " pounds.")
+
 if __name__ == '__main__':
    weight_on_planets()
